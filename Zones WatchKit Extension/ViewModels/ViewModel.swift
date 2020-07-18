f5ce30460d1bd562d511e949256c8d7a3aa94a4d
Steph Ananth <steph.ananth@gmail.com>
2020-07-17 23:21:02 -0400

    file structure

---
+++ Zones WatchKit Extension/ViewModels/ViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ViewModel.swift
+//  Zones WatchKit Extension
+//
+//  Created by Steph Ananth on 7/17/20.
+//  Copyright © 2020 Steph K. Ananth. All rights reserved.
+//
+
+import Foundation
